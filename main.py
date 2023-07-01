703792a5fc7f2403e9c73e08716d0d4d6513e9db
choijaewu <jawoo053@gmail.com>
2023-07-01 22:24:38 +0900

    basic

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 import pygame
 
-pygame.init()
+pygame.init() #초기화
 
-running = 1
+background = pygame.display.set_mode((1366,768)) #게임 창 크기
+pygame.display.set_caption("게임이름") #게임 창 이름
+
+running = 1 #게임이 실행중인가?
+
+while running:
+    for event in pygame.event.get(): #이벤트 입력
+        if event.type == pygame.QUIT: #X버튼 눌렀을 때
+            running = 0 #프로그램 종료
+
+pygame.quit() #종료
